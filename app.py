e6828d6fbb129cbfc1ffdb3b5a6d64ac27a78bed
AntoineGiraud <antoine.giraud@outlook.com>
2025-08-29 18:03:34 +0200

    feat: init

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="Ma biblothèque", page_icon="🧰")
+st.title("🧰 Ma biblothèque")
+st.markdown("*By Antoine*")
+
+st.write("On va essayer de faire l'inventaire de notre bibliothèque")
