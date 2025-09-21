fda748625d30e3dc47dfba87e5dff7d4a25e591f
AntoineGiraud <antoine.giraud@outlook.com>
2025-09-21 18:35:11 +0200

    feat(scan): on sauvegarde les codes barre de type livre

--- models/item.py
+++ models/item.py
@@ -1,16 +1,18 @@
 from sqlmodel import SQLModel, Field
 from typing import Optional, Dict
 from enum import Enum
+import requests
 
 from sqlalchemy.dialects import sqlite
 from sqlalchemy.schema import CreateTable
+from sqlalchemy import JSON
 
 from streamlit import column_config
 
 
 class MediaType(str, Enum):
     CD = "CD"
-    LIVRE = "Livre"
+    Livre = "Livre"
     BD = "BD"
     DVD = "DVD"
 
@@ -19,14 +21,23 @@ class Item(SQLModel, table=True):
     __table_args__ = {"extend_existing": True}
 
     id: Optional[int] = Field(default=None, primary_key=True)
+    # attributs principaux
     titre: str
     auteur: Optional[str]
-    annee: Optional[int] = Field(default=None, ge=1900, le=2030)
     type: MediaType
     genre: Optional[str]
+    annee: Optional[int] = Field(default=None, ge=1900, le=2030)
     note: Optional[int] = Field(default=None, ge=0, le=5)
-    # other: Optional[dict]
+    language: Optional[str]
+    description: Optional[str]
+    longueur: Optional[int]
+    # autres champs
+    editeur: Optional[str]
+    couverture: Optional[str]
+    isbn: Optional[int] = Field(default=None, ge=1e10, le=1e14)
+    other: Optional[dict] = Field(default=None, sa_type=JSON)
 
+    @staticmethod
     def get_streamlit_column_config() -> Dict:
         """Retourne la configuration des colonnes pour st.data_editor."""
 
@@ -37,8 +48,41 @@ class Item(SQLModel, table=True):
             "annee": column_config.NumberColumn("Année", min_value=1900, max_value=2030, step=1, format="%d"),
             "type": column_config.SelectboxColumn("Type", options=[e.value for e in MediaType], required=True),
             "genre": column_config.TextColumn("Genre"),
+            "longueur": column_config.NumberColumn("Longueur", min_value=0, max_value=5000, step=1, format="%d"),
             "note": column_config.NumberColumn("Note", min_value=0, max_value=5, step=1, format="%d"),
         }
+
+    @staticmethod
+    def get_book_from_isbn(isbn: int) -> SQLModel:
+        """Fetch book from isbn & init Item"""
+        url = f"https://www.googleapis.com/books/v1/volumes?q=isbn:{isbn}"
+        response = requests.get(url)
+        response.raise_for_status()
+
+        items = response.json().get("items")
+        if items:
+            item = items[0]["volumeInfo"]
+            # print(f"{item=}")
+            final = {
+                "type": "Livre",
+                "genre": item.get("categories", [None])[0],
+                "isbn": isbn,
+                "titre": item.get("title"),
+                "auteur": ", ".join(item.get("authors", [])),
+                "annee": int(item.get("publishedDate")[:4]),
+                "language": item.get("language"),
+                "longueur": item.get("pageCount"),
+                "editeur": item.get("publisher"),
+                "couverture": item.get("imageLinks", {}).get("thumbnail"),
+            }
+
+            # on garde pour l'instant au cas où les autres champs de l'api
+            for key in ["categories", "title", "authors", "publishedDate", "language", "pageCount", "publisher", "imageLinks", "industryIdentifiers", "readingModes", "panelizationSummary"]:
+                item.pop(key, None)
+            final["other"] = item
+
+            return Item(**final)
+        return None
 
 
 if __name__ == "__main__":
--- pages/1_✍_scan_code_barre.py
+++ pages/1_✍_scan_code_barre.py
@@ -1,5 +1,10 @@
 import streamlit as st
-import requests
+
+import db.crud as crud
+import db.engine as engine
+
+from sqlmodel import Session
+from models.item import Item, MediaType
 
 # ------------------------
 # CONFIG
@@ -38,62 +43,12 @@ def get_product_metadata(code: int) -> dict:
     return None
 
 
-def get_movie_metadata(title: str) -> dict:
-    """
-    Récupérer des données imdb d'un film ...\n
-    mais imposible depuis un code barre EAN-13... donc on écarte\n
-    i pour un code imdb, t pour un titre\n
-    doc: https://www.omdbapi.com/
-    """
-    url = f"http://www.omdbapi.com/?apikey={OMDB_API_KEY}&t={title}"
-    response = requests.get(url)
-    response.raise_for_status()
-
-    item = response.json()
-    print(f"{item=}")
-    if item.get("Response") == "True":
-        return dict(
-            {
-                "Titre": item.get("Title"),
-                "Année": item.get("Year"),
-                "Réalisateur": item.get("Director"),
-                "Couverture": item.get("Poster"),
-                "Type": "Film",
-            },
-            **item,
-        )
-    return None
-
-
-def get_book_metadata(isbn: int) -> dict:
-    url = f"https://www.googleapis.com/books/v1/volumes?q=isbn:{isbn}"
-    response = requests.get(url)
-    response.raise_for_status()
-
-    items = response.json().get("items")
-    if items:
-        item = items[0]["volumeInfo"]
-        image = item.get("imageLinks", {}).get("thumbnail")
-        # return item
-        return dict(
-            {
-                "Titre": item.get("title"),
-                "Auteur": ", ".join(item.get("authors", [])),
-                "Année": item.get("publishedDate"),
-                "Couverture": image,
-                "Type": "Livre",
-            },
-            **item,
-        )
-    return None
-
-
 def fetch_barcode_data(code: int) -> dict:
     """Récupérer les métadonnées de produits (livre, bd, dvd, cd ...) avec l'api la plus adaptée"""
     if not code.is_integer():
         raise "Le code doit être un entier"
     elif str(code).startswith(("978", "979", "977")):
-        return get_book_metadata(code)
+        return Item.get_book_from_isbn(code)
     else:
         return get_product_metadata(code)
 
@@ -106,25 +61,34 @@ code_input = st.number_input(
     step=1,
     value=None,
     placeholder="Type a number...",
+    key="scanned_item_isbn",
 )
 
 if st.button("Rechercher") and code_input:
     with st.spinner("🔍 Recherche en cours..."):
-        result = fetch_barcode_data(code_input)
-
-    if result:
-        type_detecte = result.get("Type")
-        type_detecte_emoji = "📚" if type_detecte == "Livre" else "📹"
-        st.subheader(f"{type_detecte_emoji} {type_detecte} `{result.get('Titre', '')}`")
-
-        if st.button("Ajouter à ma bibliothèque", type="primary", icon="💾") and code_input:
-            pass
-
-        cols = st.columns((3, 1))
-        cols[0].json(result)
-        img_url = result.get("Couverture")
-        if img_url:
-            cols[1].image(img_url, caption=result.get("Titre", ""))
-
-    else:
+        item = st.session_state["scanned_item"] = fetch_barcode_data(code_input)
+    if not item:
         st.info(f"Aucune donnée trouvée pour `{code_input}`")
+
+if st.session_state.get("scanned_item") and st.session_state["scanned_item_isbn"]:
+    item = st.session_state["scanned_item"]
+
+    type_detecte = item.type
+    type_detecte_emoji = "📚" if type_detecte == "Livre" else "📹"
+    st.subheader(f"{type_detecte_emoji} {type_detecte} `{item.titre}`")
+
+    if st.button("Ajouter à ma bibliothèque", type="primary", icon="💾") and code_input:
+        engine = engine.get_engine()
+        with Session(engine, expire_on_commit=False) as session:
+            session.add(item.copy())
+            session.commit()  # On envoie à la base de données
+
+            crud.fetch_model_into_streamlitsessionstate(session, st.session_state, Item)
+
+        st.success(f"{item.titre} ajouté")
+
+    # affichage du résultat en cours
+    col_json, col_couverture = st.columns((3, 1))
+    col_json.json(item)
+    if item.couverture:
+        col_couverture.image(item.couverture, caption=item.titre)
